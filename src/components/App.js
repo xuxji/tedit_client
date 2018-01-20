dfd1e79286872ce7947cc07040cc87e940ef1792
xiaoji.xxj <xiaoji.xxj@alibaba-inc.com>
2018-01-20 20:25:00 +0800

    init project & change ui

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Switch, NavLink, Route } from 'react-router-dom';
+import Footer from './Footer';
 import HomePage from './HomePage';
 import FuelSavingsPage from '../containers/FuelSavingsPage';
 import AboutPage from './AboutPage';
@@ -13,22 +14,59 @@ import NotFoundPage from './NotFoundPage';
 
 class App extends React.Component {
   render() {
-    const activeStyle = { color: 'blue' };
     return (
       <div>
-        <div>
-          <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
-          {' | '}
-          <NavLink to="/fuel-savings" activeStyle={activeStyle}>Demo App</NavLink>
-          {' | '}
-          <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
-        </div>
+        <section className="hero is-primary">
+          <div className="hero-head">
+            <header className="navbar">
+              <div className="container">
+                <div className="navbar-brand">
+                  <a className="navbar-item">
+                    <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo" />
+                  </a>
+                  {/* <span className="navbar-burger burger" data-target="navbarMenuHeroC">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </span> */}
+                </div>
+                <div id="navbarMenuHeroC" className="navbar-menu">
+                  <div className="navbar-end">
+                    <NavLink className="navbar-item" exact to="/" activeClassName="is-active">Home</NavLink>
+                    <NavLink className="navbar-item" to="/fuel-savings" activeClassName="is-active">Demo App</NavLink>
+                    <NavLink className="navbar-item" to="/about" activeClassName="is-active">About</NavLink>
+                    <span className="navbar-item">
+                      <a className="button is-success is-inverted">
+                        <span className="icon">
+                          <i className="fa fa-github"></i>
+                        </span>
+                        <span>Download</span>
+                      </a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </header>
+          </div>
+
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <h1 className="title">
+                Title
+              </h1>
+              <h2 className="subtitle">
+                Subtitle
+              </h2>
+            </div>
+          </div>
+        </section>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/fuel-savings" component={FuelSavingsPage} />
           <Route path="/about" component={AboutPage} />
           <Route component={NotFoundPage} />
         </Switch>
+        <Footer />
       </div>
     );
   }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>React Slingshot</h1>
-
-      <h2>Get Started</h2>
-      <ol>
-        <li>Review the <Link to="/fuel-savings">demo app</Link></li>
-        <li>Remove the demo and start coding: npm run remove-demo</li>
-      </ol>
-    </div>
+    <section className="hero is-primary">
+      <div className="hero-body">
+        <div className="container">
+          <nav className="columns">
+            <a className="column has-text-centered" href="https://bulma.io/documentation/overview/responsiveness/">
+              <span className="icon is-large" style={{ marginRight: '-15px' }}>
+                <i className="fa fa-3x fa-mobile" />
+              </span>
+              <span className="icon is-large">
+                <i className="fa fa-3x fa-tablet" />
+              </span>
+              <span className="icon is-large" style={{ marginRight: '10px' }}>
+                <i className="fa fa-3x fa-desktop" />
+              </span>
+              <p className="title is-4" style={{ marginTop: '0.5em' }}><strong>100% Responsive</strong></p>
+              <p className="subtitle">Designed for <strong>mobile</strong> first</p>
+            </a>
+            <a className="column has-text-centered" href="https://bulma.io/documentation/overview/modular/">
+              <span className="icon is-large">
+                <i className="fa fa-3x fa-cubes" />
+              </span>
+              <p className="title is-4" style={{ marginTop: '0.5em' }}><strong>Modular</strong></p>
+              <p className="subtitle">Just import what you <strong>need</strong></p>
+            </a>
+            <a className="column has-text-centered" href="https://bulma.io/documentation/columns/basics/">
+              <span className="icon is-large">
+                <i className="fa fa-3x fa-css3" />
+              </span>
+              <p className="title is-4" style={{ marginTop: '0.5em' }}><strong>Modern</strong></p>
+              <p className="subtitle">Built with <strong>Flexbox</strong></p>
+            </a>
+            <a className="column has-text-centered" href="https://github.com/jgthms/bulma">
+              <span className="icon is-large">
+                <i className="fa fa-3x fa-github" />
+              </span>
+              <p className="title is-4" style={{ marginTop: '0.5em' }}><strong>Free</strong></p>
+              <p className="subtitle">Open source on <strong>GitHub</strong></p>
+            </a>
+          </nav>
+        </div>
+      </div>
+    </section>
   );
 };
 
